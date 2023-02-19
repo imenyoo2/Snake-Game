01579b1ebaefe4607c6cfcc08e82ba3827c8a56b
ayoub <ayoubaitelaouad@gmail.com>
2023-02-19 23:47:14 +0100

    last commit before reimplementation

--- pages/components/frame.tsx
+++ pages/components/frame.tsx
@@ -1,40 +1,72 @@
-import {Render} from "./utils"
-import {MoveGroup} from "./utils"
-import {direction} from "./utils"
-import {useState, useEffect} from "react"
+import { Render, MoveGroup, direction } from "./utils";
+import { useState, useEffect } from "react";
 
+// Frame is basically the whole app component, i probably should've 
+// made it into deffrent components but i didn't notice intel i 
+// finished lol
 export default function Frame() {
   // the state of the worm
-  const [wormState, setWormState] = useState<[number[], number[]]>([[4, 5, 5, 5, 5], [5, 5, 4, 3, 2]])
-  const [currentDir, setCurrentDir] = useState<direction>("down")
-  console.log(...MoveGroup([4, 5, 5], [5, 5, 4], "up"))
-  let squirs = Render(...wormState)
+  const [wormState, setWormState] = useState<[number[], number[]]>([
+    [4, 5, 5, 5, 5],
+    [5, 5, 4, 3, 2],
+  ]);
+  const [currentDir, setCurrentDir] = useState<direction>("down");
+  console.log(...MoveGroup([4, 5, 5], [5, 5, 4], "up"));
+  let squirs = Render(...wormState);
 
   // change the currentdir of the worm to dir
-  const moveWormState = (dir: direction) => {setCurrentDir(dir)}
+  const moveWormState = (dir: direction) => {
+    setCurrentDir(dir);
+  };
 
-  useEffect (() => {
+  // implementing constant movement of the worm, by setting a function
+  // that get called every 200 ms and wormState according to currentDir
+  useEffect(() => {
     const intervalId = setInterval(() => {
-      setWormState(MoveGroup(...wormState, currentDir))
-    }, 100);
-    return () => {clearInterval(intervalId)}
-  }, [wormState])
-  
+      setWormState(MoveGroup(...wormState, currentDir));
+    }, 200);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [wormState]);
+
   return (
-  <>
-    <div className="app">
-      <div id="container">
-        {squirs}
-      </div>
-      <div className="controler">
-        <button type="button" disabled={currentDir == "down"} onClick={() => moveWormState("up")}>up</button>
-        <div className="rightLeft">
-          <button type="button"  disabled={currentDir == "right"} onClick={() => moveWormState("left")}>left</button>
-          <button type="button" disabled={currentDir == "left"} onClick={() => moveWormState("right")}>right</button>
+    <>
+      <div className="app">
+        <div id="container">{squirs}</div>
+        <div className="controler">
+          <button
+            type="button"
+            disabled={currentDir == "down"}
+            onClick={() => moveWormState("up")}
+          >
+            up
+          </button>
+          <div className="rightLeft">
+            <button
+              type="button"
+              disabled={currentDir == "right"}
+              onClick={() => moveWormState("left")}
+            >
+              left
+            </button>
+            <button
+              type="button"
+              disabled={currentDir == "left"}
+              onClick={() => moveWormState("right")}
+            >
+              right
+            </button>
+          </div>
+          <button
+            type="button"
+            disabled={currentDir == "up"}
+            onClick={() => moveWormState("down")}
+          >
+            down
+          </button>
         </div>
-        <button type="button" disabled={currentDir == "up"} onClick={() => moveWormState("down")}>down</button>
       </div>
-    </div>
-  </>
-  )
+    </>
+  );
 }
--- pages/components/utils.tsx
+++ pages/components/utils.tsx
@@ -1,100 +1,111 @@
-export type direction = "up" | "down" | "right" | "left"
+export type direction = "up" | "down" | "right" | "left";
 
 // render return the whole board with spots highlighted based on x and y
-export function Render (x: number[], y: number[]): JSX.Element[] {
+export function Render(x: number[], y: number[]): JSX.Element[] {
   let divx: JSX.Element[] = []; // array of the x's of eatch y
   let final: JSX.Element[] = [];
   // initiate divx
   for (let i = 0; i < 50; i++) {
-    divx.push(<div className="cordx"></div>)
+    divx.push(<div className="cordx"></div>);
   }
   // now we start pushing divx inside each y dev
   // we also modify the classNames of each divx element based
   // on x and y
   for (let i = 0; i < 50; i++) {
     let n = 1; // track the state of the if statment bellow, we don't want to push the same element twise
-    let buffer = [...divx]
+    let buffer = [...divx];
     for (let j = 0; j < y.length; j++) {
       if (y[j] == i) {
-        buffer[x[j]] = <div className="cordx check"></div>
-        final[i] = <div className="cordy">{buffer}</div>
+        buffer[x[j]] = <div className="cordx check"></div>;
+        final[i] = <div className="cordy">{buffer}</div>;
         n = 0; // we don't what to push twise
       }
     }
     if (n) {
-      final.push(<div className="cordy check">{divx}</div>)
+      final.push(<div className="cordy check">{divx}</div>);
     }
   }
-  return final
+  return final;
 }
 
 // Move return an array [x, y] after apply dir
 export function Move(point: number[], dir: direction): number[] {
-  let result = []
-  switch(dir) {
+  let result = [];
+  switch (dir) {
     case "up":
-      result.push(point[0])
-      result.push(point[1] - 1) // here because the way the squires rendered is reversed
-      break
+      result.push(point[0]);
+      result.push(point[1] - 1); // here because the way the squires rendered is reversed
+      break;
     case "down":
-      result.push(point[0])
-      result.push(point[1] + 1) // same reason as up
-      break
+      result.push(point[0]);
+      result.push(point[1] + 1); // same reason as up
+      break;
     case "right":
-      result.push(point[0] + 1)
-      result.push(point[1])
-      break
+      result.push(point[0] + 1);
+      result.push(point[1]);
+      break;
     case "left":
-      result.push(point[0] - 1)
-      result.push(point[1])
-      break
+      result.push(point[0] - 1);
+      result.push(point[1]);
+      break;
   }
-  return result
+  return result;
 }
 
 // RelativeMove move relative based on the target movement
 // used to move the rest of worm body
-export function RelativeMove (relative: number[], target: number[], dir: direction): [number[], direction] {
+export function RelativeMove(
+  relative: number[],
+  target: number[],
+  dir: direction
+): [number[], direction] {
   switch (dir) {
     case "up":
     case "down":
       // test if target and relative align vertically
       if (relative[0] == target[0]) {
-        return [Move(target, dir), dir]
-      // test if relative is at the right of target
-      // TODO change the comment above after testing
+        return [Move(target, dir), dir];
+        // test if relative is at the right of target
+        // TODO change the comment above after testing
       } else if (relative[0] > target[0]) {
-        return [Move(target, "right"), "right"]
+        return [Move(target, "right"), "right"];
       } else {
-        return [Move(target, "left"), "left"]
+        return [Move(target, "left"), "left"];
       }
     case "left":
     case "right":
       if (relative[1] == target[1]) {
-        return [Move(target, dir), dir]
+        return [Move(target, dir), dir];
       } else if (relative[1] > target[1]) {
-        return [Move(target, "down"), "down"]
+        return [Move(target, "down"), "down"];
       } else {
-        return [Move(target, "up"), "up"]
+        return [Move(target, "up"), "up"];
       }
   }
 }
 
 // x and y have the same length
-export function MoveGroup(x: number[], y: number[], dir: direction): [number[], number[]] {
-  let result: [number[], number[]] = [[], []]
-  let headPoint = [x[0], y[0]]
-  const movedHeadPoint = Move(headPoint, dir)
-  result[0].push(movedHeadPoint[0])
-  result[1].push(movedHeadPoint[1])
-  let newdir = dir
+export function MoveGroup(
+  x: number[],
+  y: number[],
+  dir: direction
+): [number[], number[]] {
+  let result: [number[], number[]] = [[], []];
+  let headPoint = [x[0], y[0]];
+  const movedHeadPoint = Move(headPoint, dir);
+  result[0].push(movedHeadPoint[0]);
+  result[1].push(movedHeadPoint[1]);
+  let newdir = dir;
 
   for (let i = 1; i < x.length; i++) {
-    let movedPoint: number[]
-    [movedPoint, newdir] = RelativeMove([x[i - 1], y[i - 1]], [x[i], y[i]], newdir)
-    result[0].push(movedPoint[0])
-    result[1].push(movedPoint[1])
+    let movedPoint: number[];
+    [movedPoint, newdir] = RelativeMove(
+      [x[i - 1], y[i - 1]],
+      [x[i], y[i]],
+      newdir
+    );
+    result[0].push(movedPoint[0]);
+    result[1].push(movedPoint[1]);
   }
-  return result
+  return result;
 }
-
